6f5a0579164ed5768e8973157e49baecb76491ad
Leonardo César <leocesar1977@Gmail.com>
2022-01-02 16:39:11 -0300

    Update ex071.py

--- PythonExercicios/Mundo2/ex071.py
+++ PythonExercicios/Mundo2/ex071.py
@@ -1,48 +1,24 @@
 bank = 'CEV BANK'
-nota50 = nota20 = nota10 = nota5 = nota1 = 0
-value = 0
 print('=' * 15)
 print('{:^15}'.format(bank))
 print('=' * 15)
-value = int(input('How much money do you want to get: R$'))
+value = int(input('How much money do you want to get: R$ '))
+total = value
+cedula = 50
+totalCedula = 0
 while True:
-    nota50 = value / 50
-    if nota50 > 1:
-        if value % 50 == 0:
-            print(f'Nota de 50: {nota50} e acaba')
+    if total >= cedula:
+        total -= cedula
+        totalCedula += 1
+    else:
+        print(f'Total de {totalCedula} cédulas de R$ {cedula:.2f}.')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalCedula = 0
+        if total == 0:
             break
-        if nota50 > 0:
-            print(f'Nota de 50: {nota50:.0f} e falta troco')
-    #
-    nota20 = (value % 50) / 20
-    if nota20 % 20 == 0:
-        print(f'Nota de 20: {nota20} e acaba')
-        break
-    if nota20 > 0:
-        print(f'Nota de 20: {nota20:.0f} e falta troco')
-    '''
-    #
-    nota10 = value / 10
-    if value % 10 == 0:
-        print(f'Nota de 10: {nota10}')
-        break
-    if nota10 > 0:
-        print(f'Nota de 10: {nota10}')
-    #
-    nota5 = value / 5
-    if value % 5 == 0:
-        print(f'Nota de 5: {nota5}')
-        break
-    if nota5 > 0:
-        print(f'Nota de 5: {nota5}')
-    #
-    nota1 = value / 1
-    if value % 1 == 0:
-        print(f'Nota de 10: {nota1}')
-        break
-    if nota10 > 0:
-        print(f'Nota de 50: {nota1}')
-        '''
     break
-
-
