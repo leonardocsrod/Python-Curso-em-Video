3e6d77e2f6a9f9d834b0dd962f605b82df57ad8f
Leonardo César <leocesar1977@Gmail.com>
2022-07-29 15:56:31 -0300

    Update ex081.py

--- PythonExercicios/Mundo3/ex081.py
+++ PythonExercicios/Mundo3/ex081.py
@@ -1,1 +1,17 @@
 list = []
+sum = 0
+while True:
+    n = int(input('Digit a value: '))
+    list.append(n)
+    sum += 1
+    resp = str(input('Do you want to continue[y/n]: '))
+    if resp not in 'yYnN':
+        print('The value is not valid!')
+        resp = str(input('Do you want to continue[y/n]: '))
+    if resp in 'nN':
+        break
+print(f'You digit {sum} elements.')
+print(f'The digited value were: {list}')
+
+
+
