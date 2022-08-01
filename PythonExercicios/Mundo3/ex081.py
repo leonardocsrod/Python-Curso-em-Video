851dd8ce23836cf7d9e20d92cb9c850814a2c8c0
Leonardo César <leocesar1977@Gmail.com>
2022-08-01 18:24:38 -0300

    Update ex081.py

--- PythonExercicios/Mundo3/ex081.py
+++ PythonExercicios/Mundo3/ex081.py
@@ -1,17 +1,20 @@
 list = []
-sum = 0
 while True:
-    n = int(input('Digit a value: '))
-    list.append(n)
-    sum += 1
+    list.append(int(input('Digit a value: ')))
     resp = str(input('Do you want to continue[y/n]: '))
     if resp not in 'yYnN':
         print('The value is not valid!')
         resp = str(input('Do you want to continue[y/n]: '))
     if resp in 'nN':
         break
-print(f'You digit {sum} elements.')
+print('-=' * 30)
+print(f'You digit {len(list)} elements.')
+list.sort(reverse=True)
 print(f'The digited value were: {list}')
+if 5 in list:
+    print('the value 5 is in list')
+else:
+    print('The value 5 is not in list')
 
 
 
