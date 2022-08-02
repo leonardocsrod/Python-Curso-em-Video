2b50fd1db4c4b512f5dd98f75662a1c80ad9af5d
Leonardo César <leocesar1977@Gmail.com>
2022-08-02 14:53:36 -0300

    Update ex083.py

--- PythonExercicios/Mundo3/ex083.py
+++ PythonExercicios/Mundo3/ex083.py
@@ -1,2 +1,16 @@
 list = []
-expression = int(input("Digit a expression: "))
+expression = str(input("Digit a expression: "))
+for simb in expression:
+    if simb == '(':
+        list.append('(')
+    elif simb == ')':
+        if len(list) > 0:
+            list.pop()
+        else:
+            list.append(')')
+            break
+if len(list) == 0:
+    print('Your expression is valid!')
+else:
+    print('Your expression is not valid!')
+print(f'Digited expression: {expression}')
